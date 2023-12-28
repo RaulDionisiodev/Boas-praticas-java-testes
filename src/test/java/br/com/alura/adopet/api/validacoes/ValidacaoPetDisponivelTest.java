3f60a7d98a5781c4fcc3aecb6d63adce51802d5d
CORTEX-0241\raul.dionisio_cortex <raul.dionisio@cortex-intelligence.com>
2023-12-28 18:19:49 -0300

    Utilizando a biblioteca Mockito

---
+++ src/test/java/br/com/alura/adopet/api/validacoes/ValidacaoPetDisponivelTest.java
@@ -0,0 +1,53 @@
+package br.com.alura.adopet.api.validacoes;
+
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.repository.PetRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class) // diz pra classe que precisa usar o mockito
+class ValidacaoPetDisponivelTest {
+
+    @InjectMocks //insere os mock
+    private ValidacaoPetDisponivel validacao;
+    @Mock// cria o mock da classe
+    private PetRepository petRepository;
+
+    @Mock
+    private Pet pet;
+
+    @Mock
+    private SolicitacaoAdocaoDto dto;
+
+    @Test
+    void deveriaPermitirSolicitacaoDeAdocaoPet() {
+
+        //Arrange
+        BDDMockito.given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+        BDDMockito.given(pet.getAdotado()).willReturn(false);
+
+        //Act + Assert
+        Assertions.assertDoesNotThrow(() -> validacao.validar(dto));
+    }
+
+    @Test
+    void NaoDeveriaPermitirSolicitacaoDeAdocaoPet() {
+
+        //Arrange
+        BDDMockito.given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+        BDDMockito.given(pet.getAdotado()).willReturn(true);
+
+        //Act + Assert
+        Assertions.assertThrows(ValidacaoException.class, () -> validacao.validar(dto));
+    }
+
+}
